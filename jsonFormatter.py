70829f0330f47983a6e69e3f2835866fac22c32c
Anderson Mendes <anderson2000ano@gmail.com>
2019-03-05 09:17:33 -0300

    Add jsonFormatter.py

---
+++ jsonFormatter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os, json
+
+paths = ['builds','devices.json']
+
+indents = 3
+
+def main():
+	for path in paths:
+		if os.path.isdir(path):
+			for file in os.listdir(path):
+				formatter(path+'/'+file)
+		elif os.path.isfile(path):
+  			formatter(path)
+  			
+def formatter(path):
+	if not path.endswith('.json'):
+		return
+	try:
+		data = json.loads(open(path).read())
+		outfile = open(path, 'w')
+		json.dump(data, outfile, indent=indents)
+		print(path,"...SUCCESS")
+	except:
+		print(path,"...FAILED")
+
+if __name__== "__main__":
+  main()
