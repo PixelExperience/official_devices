a103e9e42134d7211707474c63ec3677aae91e7d
Dyneteve <32036579+Dyneteve@users.noreply.github.com>
2019-10-13 17:01:15 +0000

    [PIXEL-CI]: Cleanup CI script
    Signed-off-by: Henrique Silva <jhenrique09.mcz@hotmail.com>

--- runner.sh
+++ runner.sh
@@ -3,11 +3,17 @@
 #
 ### Script to test and format our jsons
 
-ADMINS="@Dyneteve @jhenrique09 @AndroidPie9 @Hlcpereira @baalajimaestro"
-COMMIT_AUTHOR="$(git log -1 --format='%an <%ae>')"
-COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
-COMMIT_SMALL_HASH="$(git rev-parse --short HEAD)"
-COMMIT_HASH="$(git rev-parse --verify HEAD)"
+function setArgs() {
+    ADMINS="@admins"
+    BUILD_START=$(date +"%s")
+    BUILD_END=$(date +"%s")
+    DIFF=$((BUILD_END - BUILD_START))
+    GIT_CHECK="$(git status | grep "modified")"
+    COMMIT_AUTHOR="$(git log -1 --format='%an <%ae>')"
+    COMMIT_MESSAGE="$(git log -1 --pretty=%B)"
+    COMMIT_SMALL_HASH="$(git rev-parse --short HEAD)"
+    COMMIT_HASH="$(git rev-parse --verify HEAD)"
+}
 
 function sendAdmins() {
     curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001463677498&parse_mode=Markdown"
@@ -17,60 +23,64 @@ function sendMaintainers() {
     curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001287849567&parse_mode=Markdown"
 }
 
-printf "\n\n***Pixel Experience CI***\n\n"
-BUILD_START=$(date +"%s")
+function checkPullReq() {
+    printf "\n\n***Pixel Experience CI***\n\n"
 
-if [ -n "$PULL_REQUEST_NUMBER" ]; then
-    sendAdmins "\`I have recieved PR $PULL_REQUEST_NUMBER.\`"
-    sendMaintainers "\`I have recieved PR $PULL_REQUEST_NUMBER.\`"
-else
-    git checkout master  > /dev/null
-    git pull origin master  > /dev/null
-    sendAdmins "**I am building master branch job.** %0A**Commit Point:** [${COMMIT_SMALL_HASH}](https://github.com/PixelExperience/official_devices/commit/${COMMIT_HASH})"
-fi
-
-node json_tester.js
-
-RESULT=$?
-
-if [ -n "$PULL_REQUEST_NUMBER" ]; then
-    BUILD_END=$(date +"%s")
-    DIFF=$((BUILD_END - BUILD_START))
-
-    if [ "$RESULT" -eq 1 ]; then
-        echo "My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds. But its an error!"
-        sendAdmins "\`PR $PULL_REQUEST_NUMBER is failing checks. Please don't merge\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
-        sendMaintainers "\`PR $PULL_REQUEST_NUMBER is failing checks. Maintainer is requested to check it\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\` %0A%0A[PR Link](https://github.com/PixelExperience/official_devices/pull/$PULL_REQUEST_NUMBER)"
-        exit 1
+    if [ -n "$PULL_REQUEST_NUMBER" ]; then
+        sendAdmins "\`I have recieved PR $PULL_REQUEST_NUMBER.\`"
+        sendMaintainers "\`I have recieved PR $PULL_REQUEST_NUMBER.\`"
     else
-        echo "Yay! My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds.~"
-        sendAdmins "\`PR $PULL_REQUEST_NUMBER can be merged.\` %0A%0A${ADMINS} %0A%0A[PR Link](https://github.com/PixelExperience/official_devices/pull/$PULL_REQUEST_NUMBER)"
-        sendMaintainers "\`PR $PULL_REQUEST_NUMBER has passed all sanity checks. Please wait for the merge.\`"
+        git checkout master >/dev/null
+        git pull origin master >/dev/null
+    fi
+}
+
+function checkLint() {
+    if [[ "$COMMIT_MESSAGE" =~ "[PIXEL-CI]" ]]; then
+        printf "\n\n***Commit Already Linted***\n\n"
         exit 0
     fi
+}
 
-fi
+function checkJsons() {
+    node json_tester.js
+    RESULT=$?
 
-if [ "$RESULT" -eq 1 ]; then
-    BUILD_END=$(date +"%s")
-    DIFF=$((BUILD_END - BUILD_START))
-    sendAdmins "\`Someone has merged a failing file. Please look in ASAP.\` %0A%0A${ADMINS} %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
-    echo "My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds. But its an error!"
-    exit 1
-fi
+    if [ -n "$PULL_REQUEST_NUMBER" ]; then
+        if [ "$RESULT" -eq 1 ]; then
+            echo "My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds. But its an error!"
+            sendAdmins "\`PR $PULL_REQUEST_NUMBER is failing checks. Please don't merge\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
+            sendMaintainers "\`PR $PULL_REQUEST_NUMBER is failing checks. Maintainer is requested to check it\` %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\` %0A%0A[PR Link](https://github.com/PixelExperience/official_devices/pull/$PULL_REQUEST_NUMBER)"
+            exit 1
+        else
+            echo "Yay! My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds.~"
+            sendAdmins "\`PR $PULL_REQUEST_NUMBER can be merged.\` %0A%0A${ADMINS} %0A%0A[PR Link](https://github.com/PixelExperience/official_devices/pull/$PULL_REQUEST_NUMBER)"
+            sendMaintainers "\`PR $PULL_REQUEST_NUMBER has passed all sanity checks. Please wait for the merge.\`"
+            exit 0
+        fi
+    elif [ "$RESULT" -eq 1 ]; then
+        sendAdmins "\`Someone has merged a failing file. Please look in ASAP.\` %0A%0A${ADMINS} %0A%0A**Failed File:** \`$(cat /tmp/failedfile)\`"
+        echo "My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds. But its an error!"
+        exit 1
+    fi
+}
 
-GIT_CHECK="$(git status | grep "modified")"
+function pushToGit() {
+    if [ ! -n "$GIT_CHECK" ]; then
+        git reset HEAD~1
+        git add .
+        git commit -m "[PIXEL-CI]: ${COMMIT_MESSAGE}" --author="${COMMIT_AUTHOR}" --signoff
+        git remote rm origin
+        git remote add origin https://baalajimaestro:"${GH_PERSONAL_TOKEN}"@github.com/PixelExperience/official_devices.git
+        git push -f origin master
+        sendAdmins "JSON Linted and Force Pushed!"
+    fi
+    echo "Yay! My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds.~"
+}
 
-# Hack around some derps
-if [[ ! "$COMMIT_MESSAGE" =~ "[PIXEL-CI]" ]] && [ -n "$GIT_CHECK" ]; then
-      git reset HEAD~1
-    git add .
-    git commit -m "[PIXEL-CI]: ${COMMIT_MESSAGE}" --author="${COMMIT_AUTHOR}" --signoff
-    git remote rm origin
-    git remote add origin https://baalajimaestro:"${GH_PERSONAL_TOKEN}"@github.com/PixelExperience/official_devices.git
-    git push -f origin master
-    sendAdmins "JSON Linted and Force Pushed!"
-fi
-BUILD_END=$(date +"%s")
-DIFF=$((BUILD_END - BUILD_START))
-echo "Yay! My works took $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds.~"
+setArgs
+checkPullReq
+checkLint
+checkJsons
+pushToGit
+
