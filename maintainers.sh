1a841c2a294fea7dd3f742ca37d21a1741619a85
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-01-11 07:55:30 +0530

    official_devices: Add cron job to check for maintainer updates
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

---
+++ maintainers.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Copyright © 2019 PixelExperience Project
+#
+### Script to test and format our jsons
+
+ADMINS="@Hlcpereira @baalajimaestro @AndroidPie9"
+BUILD_START=$(date +"%s")
+BUILD_END=$(date +"%s")
+
+git remote rm origin
+git remote add origin https://baalajimaestro:"${GH_PERSONAL_TOKEN}"@github.com/PixelExperience/official_devices.git
+
+function sendAdmins() {
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001200493978&disable_web_page_preview=true&parse_mode=Markdown" > /dev/null
+}
+
+function sendMaintainers() {
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001287849567&disable_web_page_preview=true&parse_mode=Markdown" > dev/null
+}
+
+sendAdmins "\`I am starting monthly update check cron job\`"
+#sendMaintainers "\`I am starting monthly update check cron job\`"
+
+node monthly_update_check.js
+RESULT=$?
+
+if [ "$RESULT" -eq 1 ]; then
+  sendAdmins "\`My script seems to fail. @baalajimaestro check the CI.\`"
+  exit 1
+else
+  cat /tmp/devices_to_kick
+  #sendMaintainers "$(cat /tmp/devices_to_kick)"
+  sendAdmins "$(cat /tmp/devices_to_kick)"
+fi
---
+++ monthly_update_check.js
@@ -0,0 +1,130 @@
+let fs = require('fs');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var moment = require('moment');
+
+
+function httpGet(theUrl) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
+
+function getdevices(obj) {
+    var devices = [];
+    for (var i = 0; i < obj.length; i++)
+        devices.push(obj[i]["codename"]);
+    return devices;
+}
+
+function istensupported(devices, codename) {
+    for (var i = 0; i < devices.length; i++)
+        if (devices[i]["codename"] == codename) {
+            for (var j = 0; j < devices[i]["supported_versions"].length; j++) {
+                if (devices[i]["supported_versions"][j]["version_code"] ==
+                    "ten")
+                    return true;
+            }
+            return false;
+        }
+}
+
+function getmaintainer(devices, codename) {
+    for (var i = 0; i < devices.length; i++)
+        if (devices[i]["codename"] == codename) {
+            for (var j = 0; j < devices[i]["supported_versions"].length; j++) {
+                if (devices[i]["supported_versions"][j]["version_code"] ==
+                    "ten")
+                    return devices[i]["supported_versions"][j]["maintainer_url"]
+            }
+        }
+}
+
+function getmaintainername(devices, codename) {
+    for (var i = 0; i < devices.length; i++)
+        if (devices[i]["codename"] == codename) {
+            for (var j = 0; j < devices[i]["supported_versions"].length; j++) {
+                if (devices[i]["supported_versions"][j]["version_code"] ==
+                    "ten")
+                    return devices[i]["supported_versions"][j][
+                        "maintainer_name"
+                    ]
+            }
+        }
+}
+
+function getdeprecation(devices, codename) {
+    if (!istensupported(devices, codename))
+        return true;
+    for (var i = 0; i < devices.length; i++)
+        if (devices[i]["codename"] == codename) {
+            return devices[i]["supported_versions"][0]["deprecated"]
+        }
+}
+
+var devices_to_kick = "`These devices need to update ASAP or respond. Else they "+
+                      "will be kicked`%0A%0A";
+
+try {
+    console.log("Starting Maintainer Update Check....");
+    let data = fs.readFileSync("devices.json");
+    let json_data = JSON.parse(data);
+    var devices = getdevices(json_data);
+    let i;
+    for (i = 0; i < devices.length; i++) {
+        let url = "https://download.pixelexperience.org/ota_v3/" + devices[i] +
+            "/ten";
+        var device = JSON.parse(httpGet(url));
+        if (!device["error"]) {
+            var build_date = moment.unix(device["datetime"]);
+            var time_to_now = build_date.toNow();
+            if (time_to_now.includes("days") || time_to_now.includes("hours") ||
+                time_to_now.includes("minutes") || time_to_now.includes("day")
+            ) {
+                //console.log(devices[i] + " is okay");
+            } else {
+                if (!getdeprecation(json_data, devices[i]))
+                    if (getmaintainer(json_data, devices[i]).includes(
+                            "github") || getmaintainer(json_data, devices[i])
+                        .includes("xda")) {
+                        devices_to_kick += "- `" + getmaintainername(json_data, devices[
+                                i]) + "` maintaining `" + devices[i] +
+                            "` doesnt have tg url" + "%0A%0A";
+                    } else if (getmaintainer(json_data, devices[i]).includes(
+                        "https://t.me/")) {
+                    devices_to_kick += "- `" + getmaintainername(json_data, devices[
+                            i]) + "` @" + getmaintainer(json_data, devices[i])
+                        .slice(
+                            13, ) + " maintaining `" + devices[i] + "`%0A%0A";
+                }
+            }
+        } else {
+            if (istensupported(json_data, devices[i])) {
+                if (getmaintainer(json_data, devices[i]).includes("github") ||
+                    getmaintainer(json_data, devices[i]).includes("xda")) {
+                    devices_to_kick += "- `" + getmaintainername(json_data, devices[
+                            i]) + "` maintaining and hasnt pushed a build yet for `" + devices[i] +
+                        "` doesnt have tg url" + "%0A%0A";
+                } else if (getmaintainer(json_data, devices[i]).includes(
+                        "https://t.me/")) {
+                    devices_to_kick += "- `"+getmaintainername(json_data, devices[
+                            i]) + "` @" + getmaintainer(json_data, devices[i])
+                        .slice(13, ) + " maintaining `" + devices[i] + "` hasnt pushed a build yet.%0A%0A";
+                }
+            } else {
+                //console.log(devices[i] + " is not supported on ten");
+            }
+        }
+    }
+} catch (e) {
+    console.log(e);
+    process.exit(1);
+} finally {
+    if(devices_to_kick=="**These devices need to update ASAP or respond. Else"+
+                        "they will be kicked**%0A%0A")
+      {
+        devices_to_kick = "`No devices to kick. Keep up the good work guys!`"
+      }
+    fs.writeFileSync("/tmp/devices_to_kick", devices_to_kick)
+    console.log("Process Exited with 0")
+}
