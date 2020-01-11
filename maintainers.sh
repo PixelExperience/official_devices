b4f50a5d1fd84cf1f57db9347fc954754dea5fa3
baalajimaestro <baalajimaestro@pixelexperience.org>
2020-01-11 07:58:49 +0530

    [PIXEL-CI]: maintainers: Update chat-id
    * Was changed towards debugging
    
    Signed-off-by: baalajimaestro <baalajimaestro@pixelexperience.org>

--- maintainers.sh
+++ maintainers.sh
@@ -11,7 +11,7 @@ git remote rm origin
 git remote add origin https://baalajimaestro:"${GH_PERSONAL_TOKEN}"@github.com/PixelExperience/official_devices.git
 
 function sendAdmins() {
-    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001200493978&disable_web_page_preview=true&parse_mode=Markdown" > /dev/null
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001463677498&disable_web_page_preview=true&parse_mode=Markdown" > /dev/null
 }
 
 function sendMaintainers() {
@@ -19,7 +19,7 @@ function sendMaintainers() {
 }
 
 sendAdmins "\`I am starting monthly update check cron job\`"
-#sendMaintainers "\`I am starting monthly update check cron job\`"
+sendMaintainers "\`I am starting monthly update check cron job\`"
 
 node monthly_update_check.js
 RESULT=$?
@@ -29,6 +29,6 @@ if [ "$RESULT" -eq 1 ]; then
   exit 1
 else
   cat /tmp/devices_to_kick
-  #sendMaintainers "$(cat /tmp/devices_to_kick)"
+  sendMaintainers "$(cat /tmp/devices_to_kick)"
   sendAdmins "$(cat /tmp/devices_to_kick)"
 fi
